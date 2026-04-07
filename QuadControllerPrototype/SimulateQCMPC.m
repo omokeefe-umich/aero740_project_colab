e1877b63641a980fccf5cf7fe6aa7aee49933ff3
Owen O'Keefe <omokeefe@umich.edu>
2026-04-06 20:56:32 -0400

    Draft bounds & constraints

--- QuadControllerPrototype/SimulateQCMPC.m
+++ QuadControllerPrototype/SimulateQCMPC.m
@@ -23,6 +23,41 @@ tend = 20;          % total simulation time (s)
 dt = 0.1;              % outer loop time step (s)
 tspan = 0:dt:tend;
 
+%% Bounds
+% x (12x1): [x,y,z, phi,theta,psi, xdot,ydot,zdot, phidot,thetadot,psidot]
+% u  (4x1): [fz, tau_phi, tau_th, tau_ps]
+anglim      = deg2rad(45);   % rad
+angratelim  = deg2rad(90);   % rad/s
+torquelim   = 7;            % N-M
+speedlim    = 45;            % m/s
+Fzlim       = 75;            % N  > 5:1 T:W
+accellim    = 5;             % m/s/s
+
+xub = [inf*ones(3,1);    anglim*ones(3,1);   speedlim*ones(3,1);   angratelim*ones(3,1)];
+xlb = [-inf*ones(3,1);  -anglim*ones(3,1);  -speedlim*ones(3,1);  -angratelim*ones(3,1)];
+uub = [ Fzlim;    torquelim;  torquelim;  (1/4)*torquelim];
+ulb = [-Fzlim;   -torquelim; -torquelim; -(1/4)*torquelim];
+dxub = [ speedlim*ones(3,1);    angratelim*ones(3,1);      accellim*ones(3,1);      (1/4)*angratelim*ones(3,1)];
+dxlb = [-speedlim*ones(3,1);   -angratelim*ones(3,1);     -accellim*ones(3,1);     -(1/4)*angratelim*ones(3,1)];
+
+Xub = [dxub; xub; uub];
+Xlb = [dxlb; xlb; ulb];
+
+duub = [(2/3)*Fzlim;    (1/2)*torquelim;    (1/2)*torquelim;  (1/2)*torquelim];
+dulb = [-(2/3)*Fzlim;  -(1/2)*torquelim;   -(1/2)*torquelim; -(1/2)*torquelim];
+
+% 5:1 weight ratio for performance drone -> 6kg thrust -> ~60N max thrust
+% 4 motors -> 15 N force per motor
+% 1m across drone -> 0.5 m moment arm -> 7.5N torque limit
+% 
+% https://wrekd.com/products/t-motor-v3115-3115-900kv-motor?_pos=6&_fid=1005fc1f0&_ss=c
+% https://binsfeld.com/power-torque-speed-conversion-calculator/
+
+%% Constraints
+% Should we bring any constraints in addition to what's contained in the
+% states? They're pretty comprehensive. 
+
+
 %% MPC Parameters
 N_mpc = tend;
 dt_mpc = dt/5; 
@@ -32,11 +67,10 @@ dt_mpc = dt/5;
 x_start_qc = 0; % initial x position (m)
 y_start_qc = 0; 
 z_start_qc = 10;  
-deltax = zeros(12, 1);                    % initial linearization error (zero at start)
-x      = [x_start_qc; y_start_qc; z_start_qc; zeros(9, 1)];
+deltax = zeros(12, 1);  % initial linearization error (zero at start)
+x = [x_start_qc; y_start_qc; z_start_qc; zeros(9, 1)];
 
-% Target Vehicle
-% reference trajectory vector for target vehicle, of same form as x for quadcopter (12x1)
+% Target Vehicle trajectory
 x_start_tgt = 10; % initial x position of target vehicle (m)
 y_start_tgt = 0; 
 z_start_tgt = 0; 
@@ -65,7 +99,7 @@ Q(29:31, 13:15) = -diag([q_pos, q_pos, q_pos]);
 Q(19:21, 19:21) = diag([q_vel, q_vel, q_vel]);
 R = diag([r_fz, r_tau, r_tau, r_tau]);
 Q(25:28, 25:28) = R;
-P = 100 * Q;  % terminal cost
+P = 10 * Q;  % terminal cost
 
 %% MPC Control Loop
 for k = 1 : numel(tspan)
@@ -73,11 +107,9 @@ for k = 1 : numel(tspan)
     Ac = Jacobian(@(x) dynamics(0, x, u, params), x);
     Bc = Jacobian(@(u) dynamics(0, x + deltax, u, params), u);
 
-    % Discretize linearized system over MPC horizon
     [Ads, Bds] = Discretize_dt([dt_mpc], ones(N_mpc, 1), Ac, Bc);
 
-    % Build augmented discrete-time matrices (40x40):
-    %   [delta_x; x; u; r]_{k+1} = A_aug*[...] + B_aug*delta_u
+    % Build augmented discrete-time matrices (40x40): [delta_x; x; u; r]
     Ads_augmented = zeros(N_mpc, 40, 40);
     Bds_augmented = zeros(N_mpc, 40, 4);
     for i = 1:20
@@ -91,21 +123,14 @@ for k = 1 : numel(tspan)
                                 ];
         Bds_augmented(i, :, :) = [Bd_i;         zeros(12, 4);   eye(4);         zeros(12, 4)];
     end
-
-    % Build prediction matrices (X = M*x_aug + S*U)
     [S, M] = StackedMatrix(Ads_augmented, Bds_augmented);
 
     x_aug     = [deltax; x; u; r];
     t_horizon = tspan(k) : dt_mpc : tspan(k)+N_mpc*dt_mpc;
-
-    % Solve QP
     [H, q] = QPFormat(Q, R, P, S, M, t_horizon', x_aug, zeros(4*N_mpc, 1));
     u_mpc = quadprog(H, q);
 
-    % Apply first control increment (receding horizon)
     u = u + u_mpc(1:4);
-
-    % Advance nominal state
     x = x + deltax;
 
     Xs(:, k)    = x;
@@ -120,11 +145,8 @@ for k = 1 : numel(tspan)
     % Integrate nonlinear dynamics one outer step
     [T, X] = ode45(@(t, x) dynamics(t, x, u, params), 0:0.0001:0.01, x);
 
-    % Linearization residual for next step
     deltax = X(end, :)' - x;
-
-    % Update the trajectory of the target
-    r(1) = r(1) + 0.01 * dt;
+    % r(1) = r(1) + 0.01 * dt;
 end
 
 %% Reference Trajectory & Visualization
